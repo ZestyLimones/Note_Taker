9e868f2163d9a766249ba1d27af099896bec8d23
Micheal Limones <Micheal.Limones@du.edu>
2021-09-06 12:32:14 -0600

    added package.json and boiler-plate code to server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
